2ed94c72ce502c78feb8728a337255ac4a68808d
OuaailElaouad <2003waelelaouad@gmail.com>
2025-04-29 21:40:53 +0100

    finishing DTOs , Entity Configurations, Database Context and Module Model

---
+++ backend/catalog-service/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+// Data/ApplicationDbContext.cs
+using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+        
+        public DbSet<Formation> Formations { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
