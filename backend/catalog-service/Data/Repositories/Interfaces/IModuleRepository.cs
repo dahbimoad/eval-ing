04d430f775da1e024626f13089ab3018a95e4e51
OuaailElaouad <2003waelelaouad@gmail.com>
2025-04-29 21:45:37 +0100

    starting the services

---
+++ backend/catalog-service/Data/Repositories/Interfaces/IModuleRepository.cs
@@ -0,0 +1,12 @@
+// Data/Repositories/Interfaces/IModuleRepository.cs
+using Catalog.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data.Repositories.Interfaces
+{
+    public interface IModuleRepository : IRepository<Module>
+    {
+        Task<IEnumerable<Module>> GetModulesByFormationIdAsync(int formationId);
+    }
+}
---
+++ backend/catalog-service/Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+// Data/Repositories/Interfaces/IRepository.cs
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task SaveChangesAsync();
+    }
+}
